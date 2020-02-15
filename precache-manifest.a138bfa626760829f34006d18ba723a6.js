e3f1a6e6040bdb477715b40332da6f234249455d
alanrsoares <alanrsoars@gmail.com>
2020-02-15 04:34:45 +0000

    alanrsoares published a site update

---
+++ precache-manifest.a138bfa626760829f34006d18ba723a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a68cbeaf2e73913d867f5c5f44d714",
+    "url": "/ts-mines/index.html"
+  },
+  {
+    "revision": "93abe231d5ba13504117",
+    "url": "/ts-mines/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "391c479fa04ec0882da1",
+    "url": "/ts-mines/static/css/main.0d9c4cf7.chunk.css"
+  },
+  {
+    "revision": "93abe231d5ba13504117",
+    "url": "/ts-mines/static/js/2.ece8bd81.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ts-mines/static/js/2.ece8bd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391c479fa04ec0882da1",
+    "url": "/ts-mines/static/js/main.98d20dd3.chunk.js"
+  },
+  {
+    "revision": "d87a453fd1ae4f6a66f3",
+    "url": "/ts-mines/static/js/runtime-main.a248a427.js"
+  },
+  {
+    "revision": "d770832590f7c35d2cef0da955eca0c5",
+    "url": "/ts-mines/static/media/cool.d7708325.svg"
+  },
+  {
+    "revision": "4a335def127d31ea9b4f0544644a8832",
+    "url": "/ts-mines/static/media/eye.4a335def.svg"
+  },
+  {
+    "revision": "b21a81c5da6b663ac4ea6f8c7b49135b",
+    "url": "/ts-mines/static/media/flag.b21a81c5.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/ts-mines/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "b4c92d62521d02730f69a0ac2d9b4adf",
+    "url": "/ts-mines/static/media/mine.b4c92d62.svg"
+  },
+  {
+    "revision": "7d5904ce842ef362a2601c0476ffb035",
+    "url": "/ts-mines/static/media/skull.7d5904ce.svg"
+  },
+  {
+    "revision": "611e682853e75d1bf008c4fa5990a86b",
+    "url": "/ts-mines/static/media/thinking.611e6828.svg"
+  }
+]);
