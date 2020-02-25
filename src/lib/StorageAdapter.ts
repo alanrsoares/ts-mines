2a2d516d119caca827ee697913e53d60854ea5b1
Alan Soares <alanrsoars@gmail.com>
2020-02-25 14:51:37 +1300

    add visibility accessors

--- src/lib/StorageAdapter.ts
+++ src/lib/StorageAdapter.ts
@@ -1,17 +1,18 @@
 export default class StorageAdapter {
   private namespace = "";
+
   constructor(namespace: string) {
     this.namespace = namespace;
   }
 
-  persist<T extends {}>(value: T, path: string = "") {
+  public persist<T extends {}>(value: T, path: string = "") {
     localStorage.setItem(
       `${this.namespace}${path}`,
       JSON.stringify(value ?? null)
     );
   }
 
-  read<T>(defaultValue: T, path: string = ""): T {
+  public read<T>(defaultValue: T, path: string = ""): T {
     const value = localStorage.getItem(`${this.namespace}${path}`);
 
     return value !== null ? JSON.parse(value) : defaultValue;
